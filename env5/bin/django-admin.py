6dbb39ef65c7e1e9f4fec753a4fc4f0ebfe69366
Dhanraj.P <dhanrajp3531@gmail.com>
2018-01-07 16:29:07 +0530

    just started

---
+++ env5/bin/django-admin.py
@@ -0,0 +1,4 @@
+#!/home/dhan/PYTHON/django_chatroom/env5/bin/python3
+# EASY-INSTALL-SCRIPT: 'Django==2.0.1','django-admin.py'
+__requires__ = 'Django==2.0.1'
+__import__('pkg_resources').run_script('Django==2.0.1', 'django-admin.py')
---
+++ riot_chat/chat_client/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ChatClientConfig(AppConfig):
+    name = 'chat_client'
