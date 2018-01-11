61223998deeb7355077cb6f803da7696ded0b87d
Dhanraj.P <dhanrajp3531@gmail.com>
2018-01-12 00:03:48 +0530

    3rd bef prod

--- riot_chat/riot_chat/startup.py
+++ riot_chat/riot_chat/startup.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.path)
+import sys,os
+path2 = os.path.dirname(os.path.expanduser(os.path.abspath(__file__)))
+#print(path)
+if path2 not in sys.path:
+    sys.path.append('path2')
 def run():
     key_obj = open('key.py','r')
     key = key_obj.read().rstrip()
